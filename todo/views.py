6170a0e06ca84c836ac107fd803ae6f94f5b61ee
siobhanlgorman <siobhanlgorman@gmail.com>
2021-09-16 16:43:52 +0000

    Change say_hello() to todo_list()

--- todo/views.py
+++ todo/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("Hello!")
+def get_todo_list(request):
+    return render(request, 'todo/todo_list.html')
