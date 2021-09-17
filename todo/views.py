02fd0ef71e34aa52e321b6a7538123b529650df2
siobhanlgorman <siobhanlgorman@gmail.com>
2021-09-17 14:06:14 +0000

    Remove {{ items }} tag for query set from todo_list.html

--- todo/views.py
+++ todo/views.py
@@ -4,7 +4,7 @@ from .models import Item
 
 # Create your views here.
 def get_todo_list(request):
-    items = Item.objects.all()
+    items = Item.objects.all
     context = {
         'items': items
     }
