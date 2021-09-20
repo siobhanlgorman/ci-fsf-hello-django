82e03928aeff5b028e067f8c5c56278990c16c60
siobhanlgorman <siobhanlgorman@gmail.com>
2021-09-20 09:54:32 +0000

    Amend test_get_edit_item() in test_views.py to create a test item from models.py and use item id

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .models import Item
 
 
 class TestViews(TestCase):
@@ -14,7 +15,8 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'todo/add_item.html')
 
     def test_get_edit_item_page(self):
-        response = self.client.get('/edit/99')
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.get(f'/edit/{item.id}')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/edit_item.html')
 
