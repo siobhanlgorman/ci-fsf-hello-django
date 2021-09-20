d8b61acc88069ef7adbdc9ab02403968962be8d7
siobhanlgorman <siobhanlgorman@gmail.com>
2021-09-20 09:45:51 +0000

    Add test for add_item() to test_views

--- todo/test_views.py
+++ todo/test_views.py
@@ -8,10 +8,13 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/todo_list.html')
 
-    # def test_get_add_item_page(self):
+    def test_get_add_item_page(self):
+        response = self.client.get('/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/add_item.html')
 
-    # def test_get_edit_item_page(self):
+    def test_get_edit_item_page(self):
 
-    # def test_can_add_item(self):
+    def test_can_add_item(self):
 
-    # def test_can_toggle_item(self):
+    def test_can_toggle_item(self):
